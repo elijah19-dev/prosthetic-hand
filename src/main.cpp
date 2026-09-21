a0ec1ff415d1277b1ad18d716aede1c8ad21aad6
Elijah Moss <emoss@410medical.com>
2026-09-21 13:16:23 -0400

    Added myoware serial monitoring. to be tested with esp32

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,34 @@
 #include <Arduino.h>
 #include "config.h"
+#include "emg_sensor.h"
+
+// create myoware sensor object
+EMGSensor emgSensor(EMG_PIN);
 
 void setup() {
     Serial.begin(SERIAL_BAUD);
+
+    emgSensor.begin();
+
     Serial.println("Prosthetic hand controller initialized.");
+    Serial.println("Reading MyoWare ENV signal...");
 
     // initialize systems
 }
 
 void loop() {
 
-// read emg
-// process signal
-// update state machine
-// update servos
+int rawValue = emgSensor.readRaw();
+    uint32_t milliVolts = emgSensor.readMilliVolts();
+
+    Serial.print("Raw ADC: ");
+    Serial.print(rawValue);
+
+    Serial.print(" | Voltage: ");
+    Serial.print(milliVolts);
+
+    Serial.println(" mV");
+
+    delay(100);
 
 }
